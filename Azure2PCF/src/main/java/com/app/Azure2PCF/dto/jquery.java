62cafd13e70253e0743669d5cfc442d5b1f31c43
Namo.Shelke <Namo.Shelke@SYPUL616WZ93.synechron.com>
2021-08-16 19:41:35 +0530

    d

---
+++ Azure2PCF/src/main/java/com/app/Azure2PCF/dto/jquery.java
@@ -0,0 +1,18 @@
+package com.app.Azure2PCF.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+
+	
+	@Data
+	@NoArgsConstructor
+	@AllArgsConstructor
+
+	public class jquery {
+		private String productName;
+		private String username;
+	}
+
+
